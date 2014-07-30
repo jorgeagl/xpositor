a1bd6dbc8f03a550ef6816b7abbe8d37eb90b8c9
root <root@developer>
2014-07-30 00:05:16 -0500

    Admin 0.2

--- public/lib/admin.js
+++ public/lib/admin.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
-	jQuery.fn.CKEditorValFor = function( element_id ){
-  		return CKEDITOR.instances[element_id].getData();
-	}
+	//hover
+	$('.admin-left').find('li').click(function(e){
+		$('.admin-left').find('li').removeClass('activo');
+		var el = $(e.currentTarget);
+		$(el).addClass('activo');
 
-	$('.submit').click(function(e) {
+	});
+	//ADD.NEW
+
+	CKEDITOR.replace( 'editor1', {
+	    on: {
+	        instanceReady: function() {
+	            CKEDITOR.instances['ckeditor'].setData('<p>Hola buenas tardes</p>');
+	        },
+	        key: function() {
+	            $('.preview').html(CKEDITOR.instances['ckeditor'].getData());
+	        }
+	    }
+	} );
+
+	$('.saveAll').click(function(e) {
 	  	e.preventDefault();
 	  	
 	  	var html =  CKEDITOR.instances['ckeditor'].getData();
